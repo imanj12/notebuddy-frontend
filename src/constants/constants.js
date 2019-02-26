3810d44e98fcf21347b60a8c67823a7c50b92f70
imanj12 <imanj12@gmail.com>
2019-02-26 12:25:55 -0500

    changed editor window sizing to account for more zoomed in viewports

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 // development
-// export const URL = 'http://localhost:3000/api/v1'
+export const URL = 'http://localhost:3000/api/v1'
 
 // deployment
-export const URL = 'https://notebuddy-backend.herokuapp.com/api/v1'
+// export const URL = 'https://notebuddy-backend.herokuapp.com/api/v1'
 
